4dfa1e62bea66188bf10a57cc134ed8dbbdaef59
heebum kim <heebum9955@naver.com>
2025-07-31 08:43:42 +0900

    Initial Commit

---
+++ src/main/java/com/airoom/airoom/AiRoomApplication.java
@@ -0,0 +1,13 @@
+package com.airoom.airoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/airoom/airoom/AiRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airoom.airoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
