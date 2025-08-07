51f3a799e29d9a4f1adbcd59e23eae03eda5b34e
heebum kim <heebum9955@naver.com>
2025-08-07 20:12:17 +0900

    feat: Exam Entity 및 Enum 구현

---
+++ src/main/java/com/airoom/airoom/exam/entity/Exam.java
@@ -0,0 +1,39 @@
+package com.airoom.airoom.exam.entity;
+
+import com.airoom.airoom.common.Entity.BaseEntity;
+import com.airoom.airoom.exam.entity.value.RetryType;
+import com.airoom.airoom.exam.entity.value.Subject;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@SQLRestriction("DELETED_AT IS NULL")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE EXAM SET DELETED_AT = NOW() WHERE EXAM_NO = ?")
+public class Exam extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long examNo;
+
+    @Column(nullable = false)
+    private Integer examProblemCount;
+
+    private Timestamp examStartTime;
+
+    private Timestamp examEndTime;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RetryType examRetryType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Subject examSubject;
+}
