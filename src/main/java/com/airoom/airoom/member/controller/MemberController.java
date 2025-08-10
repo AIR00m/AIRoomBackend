b7969499c626ac103856ab713339076e1c696e77
MyeongSooJang <mmsgod0427@naver.com>
2025-08-11 08:26:47 +0900

    feat: 회원가입 로그인 생성

--- src/main/java/com/airoom/airoom/member/controller/MemberController.java
+++ src/main/java/com/airoom/airoom/member/controller/MemberController.java
@@ -1,10 +1,14 @@
-package com.airoom.airoom.member.controller;
+package com.bs.boot.myboardrest.project.config.member.controller;
 
 import com.airoom.airoom.member.model.dto.StudentDto;
+import com.airoom.airoom.member.model.dto.TeacherDto;
 import com.airoom.airoom.member.model.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,11 +16,17 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
-    @PostMapping("/join")
-    public ResponseEntity<?> login(@RequestBody StudentDto dto) {
-
+    @PostMapping("/student")
+    public ResponseEntity<String> enrollStudent(@RequestBody StudentDto studentDto) {
+        memberService.enrollStudent(studentDto);
+        return ResponseEntity.ok("학생회원 가입 완료");
     }
 
+    @PostMapping("/teacher")
+    public ResponseEntity<String> enrollStudent(@RequestBody TeacherDto teacherDto) {
+        memberService.enrollTeacher(teacherDto);
+        return ResponseEntity.ok("학생회원 가입 완료");
+    }
 
 
 
--- src/main/java/com/airoom/airoom/member/model/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.airoom.airoom.member.model.repository;
-
-import com.airoom.airoom.member.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Student,Integer> {
-}
