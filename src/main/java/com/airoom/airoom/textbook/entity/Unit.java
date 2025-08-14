71fc554afe5ad616842dea763220f16ba677be64
heebum kim <heebum9955@naver.com>
2025-08-14 17:37:13 +0900

    feat: Unit 엔티티 및 Enum 구현

--- src/main/java/com/airoom/airoom/textbook/entity/Unit.java
+++ src/main/java/com/airoom/airoom/textbook/entity/Unit.java
@@ -2,18 +2,39 @@ package com.airoom.airoom.textbook.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
-@Getter
 @Entity
+@Getter
 @Builder
-@NoArgsConstructor
+@SQLRestriction("DELETED_AT IS NULL")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE UNIT SET DELETED_AT = NOW() WHERE UNIT_NO = ?")
 @AllArgsConstructor
 public class Unit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long unitNo;
+
+    @Column(nullable = false)
     private String unitTitle;
+
+    @Column(nullable = false)
     private Integer unitNum;
+
+    @Column(nullable = false)
+    private Integer unitStart;
+
+    @Column(nullable = false)
+    private Integer unitEnd;
+
+    @Column(nullable = false)
+    private String unitPdfUrl;
+
+    @Column(nullable = false)
+    private String unitImageUrl;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEXTBOOK_NO", nullable = false)
     private Textbook textbook;
