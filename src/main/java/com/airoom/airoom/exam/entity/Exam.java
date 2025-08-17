9f320206c8c3684bac44be20e2feaa7e790a3224
heebum kim <heebum9955@naver.com>
2025-08-17 23:33:33 +0900

    feat: Exam 엔티티 수정 및 연관관계 메소드 추가

--- src/main/java/com/airoom/airoom/exam/entity/Exam.java
+++ src/main/java/com/airoom/airoom/exam/entity/Exam.java
@@ -1,6 +1,6 @@
 package com.airoom.airoom.exam.entity;
 
-import com.airoom.airoom.classroom.entity.ClassroomTeacher;
+import com.airoom.airoom.classroom.entity.Classroom;
 import com.airoom.airoom.common.Entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -8,6 +8,8 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,6 +24,9 @@ public class Exam extends BaseEntity {
     private Long examNo; //시험 고유번호
 
     @Column(nullable = false)
+    private String examName; //시험 이름
+
+    @Column(nullable = false)
     private Integer examProblemCount; //시험 문제수
 
     private LocalDateTime examStartTime; //시험 시작시간
@@ -29,6 +34,25 @@ public class Exam extends BaseEntity {
     private LocalDateTime examEndTime; //시험 종료시간
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "CLASSROOM_TEACHER_NO")
-    private ClassroomTeacher classroomTeacher; //클래스룸교사 고유번호
+    @JoinColumn(name = "CLASSROOM_NO")
+    private Classroom classroom; //클래스룸 고유번호
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "exam")
+    @Builder.Default
+    private List<ExamUnit> examUnitList = new ArrayList<>(); //시험 단원
+
+    public void addExamUnit(ExamUnit examUnit) {
+        if (examUnit != null) {
+            examUnit.getExam().removeExamUnit(examUnit);
+            examUnitList.add(examUnit);
+            examUnit.setExam(this);
+        }
+    }
+
+    public void removeExamUnit(ExamUnit examUnit) {
+        if (examUnit != null && examUnitList.remove(examUnit)) {
+            examUnit.setExam(null);
+        }
+    }
+
 }
