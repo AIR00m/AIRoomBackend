9a17c17de707c96959c09953a8992ba75f3daf03
MyeongSooJang <mmsgod0427@naver.com>
2025-08-20 18:58:05 +0900

    feat : 토큰 생성하기 시작

--- src/main/java/com/airoom/airoom/member/controller/AuthController.java
+++ src/main/java/com/airoom/airoom/member/controller/AuthController.java
@@ -1,9 +1,4 @@
 package com.airoom.airoom.member.controller;
-
-import com.airoom.airoom.classroom.entity.Classroom;
-import com.airoom.airoom.classroom.entity.ClassroomStudent;
-import com.airoom.airoom.classroom.entity.ClassroomTeacher;
-import com.airoom.airoom.classroom.model.repository.ClassroomRepository;
 import com.airoom.airoom.classroom.model.service.ClassroomService;
 import com.airoom.airoom.common.token.CookieUtility;
 import com.airoom.airoom.common.token.JWTTokenUtility;
@@ -11,17 +6,18 @@ import com.airoom.airoom.member.entity.Member;
 import com.airoom.airoom.member.model.dto.LoginRequest;
 import com.airoom.airoom.member.model.dto.SignUpRequest;
 import com.airoom.airoom.member.model.service.AuthService;
-import com.airoom.airoom.member.model.service.MemberService;
 import com.airoom.airoom.textbook.entity.Textbook;
 import com.airoom.airoom.textbook.model.service.TextbookService;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -32,6 +28,9 @@ public class AuthController {
 
     private final AuthService authService;
     private final TextbookService textbookService;
+    private final ClassroomService classroomService;
+    private final JWTTokenUtility jwtUtility;
+    private final CookieUtility cookieUtility;
 
     //학생 회원가입
     @PostMapping("/signup/student")
@@ -61,6 +60,44 @@ public class AuthController {
                 .body(Map.of("memberId", member.getMemberId(), "textbooks", textbooks));
 
     }
+
+    @PostMapping("/token")
+    public ResponseEntity<?> createTokenOnTextbookClick(@Valid @RequestBody String memberId, Long textbookNo) {
+        // 프론트에서 아이디를 넘겨준것으로 맴버를 반환
+        Member member = authService.searchById(memberId);
+        // 역할 확인
+        boolean isTeacher = member.getMemberType().toString().equals("TEACHER");
+        Long memberNo = member.getMemberNo();
+        // 클래스룸 고유번호
+        Map<String,Object> classroomClaims = new HashMap<>();
+
+        Long classroomNo  = classroomService.getClassroomNo(memberId,textbookNo);
+
+        classroomClaims.put("classroomNo",classroomNo);
+        if (isTeacher) {
+            List<Long> classroomTeacherNos = classroomService.getClassroomTeacherNosByMemberNo(memberNo);
+            classroomClaims.put("classroomTeacherNos",classroomTeacherNos);
+        } else {
+            List<Long> classroomStudentNos = classroomService.getClassroomStudentNosByMemberNo(memberNo);
+            classroomClaims.put("classroomStudentNos",classroomStudentNos);
+        }
+
+        // 3. 서버에서 토큰을 발급
+        String accessToken = jwtUtility.createAccessToken(member, isTeacher, classroomClaims);
+        String refreshToken = jwtUtility.createRefreshToken(memberId, isTeacher);
+
+        //3-1 Redis에는 (Time To Live)기능이 존재하여
+
+        ResponseCookie cookie = cookieUtility.refreshTokenCookie(refreshToken);
+        // 쿠키에 담는 것도 좋지만 프론트에서 localstorage에 담는것도 생각해보는 것을 추천
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(Map.of("Access_Token", accessToken));
+
+    }
+
+
 
 
 }
