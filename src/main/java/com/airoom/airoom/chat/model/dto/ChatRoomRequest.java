1d231b1ecd34fce258c218a6424b3e32d2de3b97
sysypp <parkseoyoung0915@gmail.com>
2025-08-24 23:43:22 +0900

    feat: 채팅 DTO 구현

---
+++ src/main/java/com/airoom/airoom/chat/model/dto/ChatRoomRequest.java
@@ -0,0 +1,15 @@
+package com.airoom.airoom.chat.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatRoomRequest {
+    private Long classroomTeacherNo;
+    private Long classroomStudentNo;
+}
