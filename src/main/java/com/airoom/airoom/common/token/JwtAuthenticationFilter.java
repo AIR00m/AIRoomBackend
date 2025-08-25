852975936094c5ec08b2e5b106874d6416159265
Jihyuck <wlgur4772@gmail.com>
2025-08-26 08:55:57 +0900

    Feat: 개발용으로 /api/aichat/** added to whitelist (나중에 세부 조정 필요)

--- src/main/java/com/airoom/airoom/common/token/JwtAuthenticationFilter.java
+++ src/main/java/com/airoom/airoom/common/token/JwtAuthenticationFilter.java
@@ -33,6 +33,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             "/install/**",
             "/forensic/**",
             "/api/forensic/**",
+            "/api/aichat/**",
             "/agent-required/**",
             "/auth/**",
             "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html"); // 필터에 적용 받지 않을 위치 경로를 추가
