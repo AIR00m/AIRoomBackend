061ab86b1f80ef0543bf42d3bad2b33cd986b846
nunus18 <wosik123@naver.com>
2025-08-31 11:41:35 +0900

    feat: 과제 제출, 평가 시 대상에게 알림 전송 구현

--- src/main/java/com/airoom/airoom/notification/entity/value/NotificationType.java
+++ src/main/java/com/airoom/airoom/notification/entity/value/NotificationType.java
@@ -8,15 +8,11 @@ public enum NotificationType {
 
     // 학생 알림 (과제/평가 관련)
     NEW_ASSIGNMENT("[과제] 새로운 과제가 출제되었습니다", "/assignment"),
-    NEW_GROUP_ASSIGNMENT("[모둠과제] 새로운 과제가 출제되었습니다", "/assignment"),
+    NEW_GROUP_ASSIGNMENT("[모둠과제] 새로 운과제가 출제되었습니다", "/assignment"),
     NEW_EXAM("[평가] 새로운 평가가 생성되었습니다", "/exam"),
     NEW_MATERIAL("[학습자료] 새로운 학습 자료가 등록되었습니다", "/subjectboard/list"),
     ASSIGNMENT_GRADED("[과제] 과제가 채점되었습니다", "/assignment"),
-
-    // 선생님 알림
-    ASSIGNMENT_SUBMITTED("[과제] 학생이 과제를 제출했습니다", "/assignment"),
-    NEW_GROUP_POST("[모둠게시판] 새 글이 작성되었습니다", "/assignment/groupboard"),
-    NEW_GROUP_COMMENT("[모둠게시판] 새 댓글이 작성되었습니다", "/assignment/groupboard");
+    ASSIGNMENT_SUBMITTED("[과제] 학생이 과제를 제출했습니다", "/assignment");
 
     private final String message;
     private final String location;
@@ -25,7 +21,6 @@ public enum NotificationType {
         this.message = msg;
         this.location = redirectLocation;
     }
-
 
     public String getMsg() {
         return message;
