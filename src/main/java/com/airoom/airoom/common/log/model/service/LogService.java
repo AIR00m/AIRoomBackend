55688bc6a548a0bf0a5eb3b0d05a2553c0e5a347
poj23 <poj2365@gmail.com>
2025-08-31 16:32:13 +0900

    fix : logstash.conf 수정

--- src/main/java/com/airoom/airoom/common/log/model/service/LogService.java
+++ src/main/java/com/airoom/airoom/common/log/model/service/LogService.java
@@ -36,12 +36,11 @@ public class LogService {
             logData.put("classroomNo", request.classroomNo());
             logData.put("problemNo", request.problemNo() != null ? request.problemNo() : 0L);
 
+            logData.put("solvingTime", request.solvingTime() != null ? request.solvingTime() : 0L);
+
             // 타임스탬프 변환
             String isoTimestamp = convertTimestampToISO(request.timestamp());
-            String isoSolvingTime = convertTimestampToISO(request.solvingTime());
-            logData.put("solvingTime", isoSolvingTime);
             logData.put("timestamp", isoTimestamp);
-            logData.put("processedAt", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 
             // 이상행위 카운트
             logData.put("controlVCount", request.controlVCount());
