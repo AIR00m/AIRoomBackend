9a18be73ccba14999d82544253b65b8409246abf
heebum kim <heebum9955@naver.com>
2025-08-31 20:25:07 +0900

    hotfix: 교사 페이지 우리반 학습 현황 관리 학습시간 및 문제풀이 수 로직(쿼리) 수정

--- src/main/java/com/airoom/airoom/statistic/model/repository/LearningSummaryRepository.java
+++ src/main/java/com/airoom/airoom/statistic/model/repository/LearningSummaryRepository.java
@@ -40,41 +40,45 @@ public interface LearningSummaryRepository extends JpaRepository<LearningSummary
     StudentLearningSummaryResponse findByClassroomStudentAndTypeAndRange(List<Long> studentNos, SummaryType summaryType, LocalDate lsStartDate, LocalDate lsEndDate);
 
     @Query("""
-                select new com.airoom.airoom.statistic.model.dto.ClassroomLearningSummaryAllResponse(
-                    cs.classRoomStudentNo,
-                    cs.student.memberName,
+            select new com.airoom.airoom.statistic.model.dto.ClassroomLearningSummaryAllResponse(
+                cs.classRoomStudentNo,
+                cs.student.memberName,
             
-                    (select coalesce(sum(ls.lsTotalLearningTimeMs),0)
-                     from LearningSummary ls
-                     where ls.id.lsClassroomStudentNo = cs.classRoomStudentNo),
+                (select coalesce(sum(ls.lsTotalLearningTimeMs),0)
+                 from LearningSummary ls
+                 where ls.id.lsClassroomStudentNo = cs.classRoomStudentNo
+                   and ls.id.lsType = com.airoom.airoom.statistic.entity.value.SummaryType.DAILY),
             
-                    (select coalesce(sum(ls.lsTotalProblemsSolved),0)
-                     from LearningSummary ls
-                     where ls.id.lsClassroomStudentNo = cs.classRoomStudentNo),
+                (select coalesce(sum(ls.lsTotalProblemsSolved),0)
+                 from LearningSummary ls
+                 where ls.id.lsClassroomStudentNo = cs.classRoomStudentNo
+                   and ls.id.lsType = com.airoom.airoom.statistic.entity.value.SummaryType.DAILY),
             
-                    (select coalesce(sum(ls.lsTotalCorrectProblems),0)
-                     from LearningSummary ls
-                     where ls.id.lsClassroomStudentNo = cs.classRoomStudentNo),
+                (select coalesce(sum(ls.lsTotalCorrectProblems),0)
+                 from LearningSummary ls
+                 where ls.id.lsClassroomStudentNo = cs.classRoomStudentNo
+                   and ls.id.lsType = com.airoom.airoom.statistic.entity.value.SummaryType.DAILY),
             
-                    (select coalesce(sum(p.progressLastPage),0)
-                     from Progress p
-                     where p.classroomStudent.classRoomStudentNo = cs.classRoomStudentNo),
+                (select coalesce(sum(p.progressLastPage),0)
+                 from Progress p
+                 where p.classroomStudent.classRoomStudentNo = cs.classRoomStudentNo),
             
-                    (select coalesce(sum(h.homeworkScore),0)
-                     from Homework h
-                     where h.assignTarget.targetNo = cs.classRoomStudentNo
-                       and h.assignTarget.groupAssignType = false),
+                (select coalesce(sum(h.homeworkScore),0)
+                 from Homework h
+                 where h.assignTarget.targetNo = cs.classRoomStudentNo
+                   and h.assignTarget.groupAssignType = false),
             
-                    (select coalesce(count(h.homeworkScore),0)
-                     from Homework h
-                     where h.assignTarget.targetNo = cs.classRoomStudentNo
-                       and h.assignTarget.groupAssignType = false)
-                )
-                from ClassroomStudent cs
-                where cs.classRoomStudentNo in :studentNos
-                order by cs.student.memberName
+                (select coalesce(count(h.homeworkScore),0)
+                 from Homework h
+                 where h.assignTarget.targetNo = cs.classRoomStudentNo
+                   and h.assignTarget.groupAssignType = false)
+            )
+            from ClassroomStudent cs
+            where cs.classRoomStudentNo in :studentNos
+            order by cs.student.memberName
             """)
     List<ClassroomLearningSummaryAllResponse> findByClassroomStudentAll(List<Long> studentNos);
+
 
 
     Optional<LearningSummary> findTopById_LsClassroomStudentNoOrderByCreatedAtDesc(Long csNo);
