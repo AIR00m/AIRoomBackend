81db40cf14128f4bfba001691fa9f4f819a1b118
heebum kim <heebum9955@naver.com>
2025-08-31 21:34:01 +0900

    feat: KafkaConfig Consumer 및 배치처리 추가설정

--- src/main/java/com/airoom/airoom/common/config/KafkaConfig.java
+++ src/main/java/com/airoom/airoom/common/config/KafkaConfig.java
@@ -3,10 +3,15 @@ package com.airoom.airoom.common.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Bean
@@ -27,5 +32,18 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaBatchFactory(
+            ConsumerFactory<String, String> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        factory.setBatchListener(true);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        return factory;
     }
 }
